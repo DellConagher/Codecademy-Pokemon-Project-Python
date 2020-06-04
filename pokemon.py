7783cce978cec7a7a6d231d36061ae8731ef3692
Tyler <tylerclifton94@gmail.com>
2020-06-04 15:27:06 -0400

    Re-designed systems

--- pokemon.py
+++ pokemon.py
@@ -1,3 +1,13 @@
+import math
+
+poketypes = {
+        "Grass" : {"Grass" : 0.5, "Fire" : 0.5, "Water" : 2},
+        "Fire" : {"Grass" : 2, "Fire" : 0.5, "Water" : 0.5},
+        "Water" : {"Grass" : 0.5, "Fire" : 2, "Water" : 0.5}        
+        }
+
+
+
 class Pokemon:
     def __init__(self, name, level, type):
         self.name = name
@@ -7,148 +17,126 @@ class Pokemon:
         self.max_health = level * 5
         self.health = self.max_health
         self.type = type
-        self.is_knocked_out = False
+        self.faint_status = False
 
+    def __str__(self):
+        return f"{self.name}, {self.level}, {self.type}"
 
-    def knock_out(self):
+    def print_stats(self):
+        print(f"Name: {self.name}, Level: {self.level}, Max Health: {self.max_health}, Current Health: {self.health}.")
+
+    def faint(self):
         if self.health <= 0:
-            self.is_knocked_out = True
-            print(f"{self.name} has been knocked out!")
+            self.faint_status = True
+            print(f"{self.name} has fainted!")
         else:
-            self.is_knocked_out = False
-            print("not knocked out")                                                                           #remove this when finished testing
-        
-
-    def revive(self):
-        if self.is_knocked_out == True:
-            self.health = self.max_health / 2
-            self.is_knocked_out = False
-            print(f"{self.name} has been revived and has {self.health} health!")
-        else:
-            print(f"{self.name} is not knocked out")
-
-
-
+            self.faint_status = False
+            print("Has health.")    #testing purposes only
+    
     def lose_health(self, health_lost):
         health_remaining = self.health - health_lost
         self.health = health_remaining
+        print(f"{self.name} lost {health_lost} health!  {self.name} has {self.health} health remaining.")
         if self.health <= 0:
             self.health = 0
-        print(f"{self.name} lost {health_lost} health. {self.name} now has {self.health} health")
-        if self.health <= 0:
-            self.knock_out()        
-        
-
+            self.faint()
+    
     def gain_health(self, health_gained):
         if self.health + health_gained > self.max_health:
             self.health = self.max_health
         else:
             self.health += health_gained
         print(f"{self.name} gained {health_gained}. {self.name} now has {self.health} health")
-        
 
-    
-    def attack(self, other_pokemon):
-        types = ["grass", "fire", "water"]
-        damage = 0
-        if other_pokemon.health > 0:
-            if (self.type is types[0] and other_pokemon.type is types[1]) or (self.type is types[1] and other_pokemon.type is types[2]) or (self.type is types[2] and other_pokemon.type is types[0]):
-                damage = self.level / 2
-                print(f"{self.name} attacks {other_pokemon.name} for {damage} damage!")
-                print("It wasn't very effective.")
-            elif (self.type is types[0] and other_pokemon.type is types[2]) or (self.type is types[1] and other_pokemon.type is types[0]) or (self.type is types[2] and other_pokemon.type is types[1]):
-                damage = self.level * 2
-                print(f"{self.name} attacks {other_pokemon.name} for {damage} damage!")
-                print("It was super effective!")
-            else:
-                damage = self.level
-                print(f"{self.name} attacks {other_pokemon.name} for {damage} damage!")
-            other_pokemon.lose_health(damage)
+    def revive(self):
+        if self.faint_status == True:
+            self.health = self.max_health / 2
+            self.faint_status = False
+            print(f"{self.name} has been revived and has {self.health} health!")
         else:
-            print("Can't attack a pokemon that is knocked out!")
-        
+            print("Can't use that here.")
 
-    def printStats(self):
-        print(f"Name: {self.name}, Level: {self.level}, Max Health: {self.max_health}, Current Health: {self.health}.")
+    def attack(self, other):
+        multiplier = poketypes[self.type][other.type]
+        attack = math.floor(self.level * multiplier)
+        other.health -= attack
+        print(f"{self.name} attacked {other.name} for {attack} damage! {other.name} has {other.health} health remaining.")
+        if multiplier == 0.5:
+            print("It's not very effective.")
+        elif multiplier == 2:
+            print("It's super effective!")
 
-    def __repr__(self):
-        return f"{self.name}"
+class GrassType(Pokemon):
+    def __init__(self, name, level):
+        super().__init__(name, level, "Grass")
 
-    def test(self):
-        print("Hello this is test")
+class FireType(Pokemon):
+    def __init__(self, name, level):
+        super().__init__(name, level, "Fire")
 
+class WaterType(Pokemon):
+    def __init__(self, name, level):
+        super().__init__(name, level, "Water")
 
 class Trainer:
-    def __init__(self, name, numPotions, numRevives, pokeTeam):
+    def __init__(self, name, poke_team, num_potions, num_revives):
         self.name = name
-        self.numPotions = numPotions
-        self.numRevives = numRevives
-        self.pokeTeam = pokeTeam
-        self.current_pokemon = 0
+        self.poke_team = poke_team[:6] if len(poke_team) > 6 else poke_team
+        self.current_poke = self.poke_team[0]
+        self.num_potions = 3
+        self.num_revives = 1
 
-    def usePotion(self):
-        if self.numPotions > 0:
-            print(f"{self.name} used a Potion!")
-            self.pokeTeam[self.current_pokemon].gain_health(20)
+    def __repr__(self):
+        print(f"{self.name} currently has the following pokemon:")
+        for poke in self.poke_team:
+            print(poke)
+        return f"Current pokemon: {self.current_poke}"
+
+    def attack(self, other):
+        if self.current_poke.faint_status == False:
+            self.current_poke.attack(other.current_poke)
         else:
-            print(f"{self.name} doesn't have any potions left!")
+            print(f"{self.current_poke.name} has fainted, choose another pokemon, {self.name}.")
 
-    def useRevive(self):
-        if self.numRevives > 0:
-            if self.pokeTeam[self.current_pokemon].health <= 0:
-                print(f"{self.name} used a Revive!")
-                self.pokeTeam[self.current_pokemon].revive()
-            else:
-                print("Can't use revive on pokemon that aren't knocked out.")
+    def use_potion(self):
+        if self.num_potions > 0:
+            print(f"{self.name} used a potion on {self.current_poke}.")
+            self.current_poke.gain_health(20)
         else:
-            print(f"{self.name} doesn't have any revives left!")
+            print(f"{self.name} doesn't have any potions left.")
 
-    def attack_other_trainer(self, other_trainer):
-        self.pokeTeam[self.current_pokemon].attack(other_trainer.pokeTeam[other_trainer.current_pokemon])
-
-    def switch_Pokemon(self):
-        poke_choice = input("Choose the pokemon you would like to switch to: ")
-        poke_choice = poke_choice.upper()
-        no_health = "You can't send out a pokemon with no health."
-
-        if poke_choice == "BULBASAUR":
-            index = self.pokeTeam.index(a)
-            if self.pokeTeam[index].health > 0:
-                self.current_pokemon = index
-                print("You sent out Bulbasaur!")
-            else:
-                print(no_health)
-        elif poke_choice == "CHARMANDER":
-            index = self.pokeTeam.index(b)
-            if self.pokeTeam[index].health > 0:
-                self.current_pokemon = index
-                print("You sent out Charmander!")
-            else:
-                print(no_health)
-        elif poke_choice == "SQUIRTLE":
-            index = self.pokeTeam.index(c)
-            if self.pokeTeam[index].health > 0:
-                self.current_pokemon = index
-                print("You sent out Squirtle!")
-            else:
-                print(no_health)
+    def use_revive(self):
+        if self.num_revives > 0:
+            print(f"{self.name} used a revive on {self.current_poke}.")
+            self.current_poke.revive()
         else:
-            print("That's not a vaild Pokemon.")
+            print(f"{self.name} doesn't have any revives left.")
 
-    def display_current_pokemon(self):
-        print(f"Your current Pokemon is {self.pokeTeam[self.current_pokemon].name}.")
-
-
-
-
-a = Pokemon("Bulbasaur", 5, "grass")
-b = Pokemon("Charmander", 5, "fire")
-c = Pokemon("Squirtle", 5, "water")
-
-
-trainer_one = Trainer("Red", 3, 1, [a, b, c])
-trainer_two = Trainer("Blue", 3, 1, [c, b, a])
+    def switch_poke(self, num):
+        if self.poke_team[num] == self.current_poke:
+            print(f"{self.name} cannot switch to currently active pokemon.")
+        elif self.poke_team[num].faint_status == False:
+            print(f"{self.name} switched {self.current_poke} with {self.poke_team[num]}.")
+            self.current_poke = self.poke_team[num]
+        elif self.poke_team[num].faint_status == True:
+            print(f"{self.poke_team[num]} has no health.  Choose another pokemon, {self.name}.")
+        else:
+            print("Invaild Action")
 
 
-trainer_two.switch_Pokemon()
-trainer_two.display_current_pokemon()
+Bulbasaur = GrassType("Bulbasaur", 5)
+Charmander = FireType("Charmander", 5)
+Squirtle = WaterType("Squirtle", 5)
+
+Red = Trainer("Red", [Bulbasaur, Charmander, Squirtle], 3, 1)
+
+Blue = Trainer("Blue", [Squirtle, Charmander, Bulbasaur], 3, 1)
+
+#print(Red)
+#print(Blue)
+
+Red.attack(Blue)
+Blue.attack(Red)
+Red.attack(Blue)
+Blue.switch_poke(1)
+Red.attack(Blue)
